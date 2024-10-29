a75e959e398dff2829db879c6c2243fb297f6390
v2k2000 <shiun147@gmail.com>
2024-10-29 11:21:33 +0900

    저장안되어서 해결하려다가 포기

--- sidepanel.js
+++ sidepanel.js
@@ -1,28 +1,8 @@
-console.log('sidepanel.js');
-
-
-// IndexedDB에 저장할 수 있도록 데이터베이스와 객체 저장소를 설정하는 함수
-function initializeDB() {
-    return new Promise((resolve, reject) => {
-      const request = indexedDB.open('PageStorageDB', 1);
-      request.onupgradeneeded = (event) => {
-        const db = event.target.result;
-        if (!db.objectStoreNames.contains('pages')) {
-          db.createObjectStore('pages', { keyPath: 'id', autoIncrement: true });
-        }
-      };
-      request.onsuccess = (event) => {
-        resolve(event.target.result);
-      };
-      request.onerror = (event) => {
-        reject(event.target.error);
-      };
-    });
-  }
-  
-  // 현재 페이지의 정보를 IndexedDB에 저장하는 함수
-  async function saveCurrentPage() {
+async function saveCurrentPage() {
+    console.log('Initializing database...');
     const db = await initializeDB();
+    console.log('Database initialized:', db);
+  
     const transaction = db.transaction('pages', 'readwrite');
     const store = transaction.objectStore('pages');
   
@@ -32,15 +12,16 @@ function initializeDB() {
       htmlContent: document.documentElement.outerHTML
     };
   
+    console.log('Adding page data to store:', pageData);
     store.add(pageData);
+  
     transaction.oncomplete = () => {
+      console.log('Transaction complete');
       alert('페이지가 성공적으로 저장되었습니다.');
     };
     transaction.onerror = () => {
+      console.error('Transaction error', transaction.error);
       alert('페이지 저장 중 오류가 발생했습니다.');
     };
   }
-  
-  // 버튼 클릭 이벤트 리스너 설정
-  document.getElementById('save-page-button').addEventListener('click', saveCurrentPage);
   
